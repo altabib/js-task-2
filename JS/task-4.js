00ddeb310585179d2546b17878bc4e28962a714d
Ivan <109656369+altabib@users.noreply.github.com>
2025-04-14 15:57:00 +0300

    task-2-4

--- JS/task-4.js
+++ JS/task-4.js
@@ -1,1 +1,22 @@
+const formValue = document.querySelector(".login-form");
+const inputEmail = document.querySelector("input[name=email]");
+const inputPassword = document.querySelector("input[name=password]");
 
+formValue.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const trimValue = inputEmail.value.trim();
+    const trimPassword = inputPassword.value.trim();
+
+if (trimValue === "" || trimPassword === "") {
+    alert("Please fill in all the fields!");
+    return;
+}
+    const formData = {
+        Login: trimValue,
+        Password: trimPassword
+}
+    
+    console.log(formData);
+    formValue.reset();
+
+});
