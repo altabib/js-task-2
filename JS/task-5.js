0d09c71feaeab9a3997263f4bf78425c718a5579
Ivan <109656369+altabib@users.noreply.github.com>
2025-04-15 17:45:44 +0300

    task-2-5

--- JS/task-5.js
+++ JS/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector('body');
+const colorButton = document.querySelector('.change-color');
+const colorName = document.querySelector('.color');
+colorButton.addEventListener('click', changeColor);
+
+function changeColor() { 
+  body.style.backgroundColor = getRandomHexColor();
+  colorName.textContent = body.style.backgroundColor;
+}
